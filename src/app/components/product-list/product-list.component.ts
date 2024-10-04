28aaf6e2125606c3509ab50ee0f811aa2534fef3
Fredi Román <froman@mushroomsoft-it.com>
2024-10-03 20:01:59 -0500

    feat: add the ability to search in the products list, add a basic pagination.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -7,17 +7,28 @@ import { Product } from '../../models/product';
 import { GenericSubjectService } from '../../subjects/generic-subject.service';
 import { ModalComponent } from '../generic/modal/modal.component';
 import { MIconComponent } from '../generic/m-icon/m-icon.component';
+import { FormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, ModalComponent, MIconComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ModalComponent,
+    MIconComponent,
+    FormsModule,
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent implements OnInit {
-  public productList: FinancialProduct[] = [];
+  private productList: FinancialProduct[] = [];
   public selectedProduct: FinancialProduct | undefined;
+  public itemsPerPage: number = 5;
+  public currentPage: number = 0;
+  public filterString: string = '';
 
   constructor(
     private productService: ProductService,
@@ -26,6 +37,26 @@ export class ProductListComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.productList = await this.productService.listProducts();
+  }
+
+  /** Returns the number of pages for displaying the paginator. */
+  get numberOfPages() {
+    return Math.ceil(this.filteredList.length / this.itemsPerPage);
+  }
+
+  /** Returns  a paginated list of products. */
+  get paginatedList() {
+    const offset = this.currentPage * this.itemsPerPage;
+    return this.filteredList.slice(offset, offset + this.itemsPerPage);
+  }
+
+  /** Returns a filtered list of products. */
+  get filteredList() {
+    return this.productList.filter(
+      (r) =>
+        r.name.toLowerCase().includes(this.filterString.toLowerCase()) ||
+        r.description.toLowerCase().includes(this.filterString.toLowerCase())
+    );
   }
 
   /** Calls the Subject to send a product to the update product view. */
